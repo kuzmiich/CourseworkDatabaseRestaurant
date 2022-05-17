c74542bb63e7517b8e496f4338fb7410d604ae55
240Dell <240Dell@IVANK>
2022-05-17 23:23:18 +0300

    Fixed GoodsLogsByInterval function minor updates in SeedData

--- Restaurant.Database/Procedures/GetGoodsLogsByInterval.sql
+++ Restaurant.Database/Procedures/GetGoodsLogsByInterval.sql
@@ -1,21 +1,21 @@
-﻿CREATE PROCEDURE GetGoodsLogsByDateInterval 
+﻿CREATE PROCEDURE GetGoodsLogsByDateInterval
 	@FromDate date,
 	@ToDate date
 AS
 BEGIN
 	SET NOCOUNT ON; -- Delete the numbers of rows in result statement
-	SELECT dbo.Deliveries.DeliveryName,
+	SELECT dbo.Delivery.DeliveryName,
+		dbo.Manufacturers.ManufacturerName,
 		dbo.GoodsLogs.DateOfRegistration,
-		dbo.Manufactures.ManufacturerName,
 		dbo.Goods.GoodsName,
 		dbo.Goods.Count AS GoodsCount,
+		dbo.Goods.Count * dbo.Goods.Price AS GoodsTotalPrice,
 		dbo.Providers.ProviderName
 	FROM dbo.GoodsLogs 
-		INNER JOIN dbo.Deliveries ON dbo.GoodsLogs.DeliveryId = dbo.Deliveries.Id
+		INNER JOIN dbo.Delivery ON dbo.GoodsLogs.DeliveryId = dbo.Delivery.Id
 		INNER JOIN dbo.Goods ON dbo.GoodsLogs.GoodsId = dbo.Goods.Id
-		INNER JOIN dbo.Manufacturer ON dbo.Goods.ManufacturerId = dbo.Manufacturers.Id
+		INNER JOIN dbo.Manufacturers ON dbo.Goods.ManufacturerId = dbo.Manufacturers.Id
 		INNER JOIN dbo.Providers ON dbo.GoodsLogs.ProviderId = dbo.Providers.Id
 	WHERE DateOfRegistration between @FromDate and @ToDate
 	ORDER BY DateOfRegistration ASC
 END
-GO 
